d03e8f13828ca772536fc50f2bdce60421116400
pushpendre <pushpendre@jhu.edu>
2015-12-30 15:47:26 -0500

    nmt from other branch.

--- session3/train_nmt.py
+++ session3/train_nmt.py
@@ -1,49 +1,69 @@
-import numpy
-import os
+#!/usr/bin/env python
+#-------------------#
+# Dataset Creation. #
+#-------------------#
+# train = TextIterator(datasets[0], datasets[1],
+# data_iterator.TextIterator
+# def next()
+#   read from source file and map to word index
+#   source.append(ss)
+#   target.append(tt)
+#   return source, target
+#   ss = self.source.readline()
+#   If there is an empty line then set end_of_data = True
+#     if ss == "": raise IOError except IOError: self.end_of_data = True
+#   Now Strip and split ss
+#   Now map
+#   ss = [(self.source_dict[w] if w in self.source_dict else 1)
+#          for w in ss]
+# Basically 1 is the index of OOV token.
+DATASETS = ['lower_string.train.tok',
+            'upper_string.train.tok']
+VALID_DATASETS = ['lower_string.dev.tok',
+                  'upper_string.dev.tok']
+#----------------------#
+# Dictionary Creation. #
+#----------------------#
+DICTIONARIES = ['dict.pkl', 'dict.pkl']
 
-from nmt import train
-
-def main(job_id, params):
-    print params
-    basedir = '/data/lisatmp3/firatorh/nmt/europarlv7'
-    validerr = train(saveto=params['model'][0],
-                                        reload_=params['reload'][0],
-                                        dim_word=params['dim_word'][0],
-                                        dim=params['dim'][0],
-                                        n_words=params['n-words'][0],
-                                        n_words_src=params['n-words'][0],
-                                        decay_c=params['decay-c'][0],
-                                        clip_c=params['clip-c'][0],
-                                        lrate=params['learning-rate'][0],
-                                        optimizer=params['optimizer'][0],
-                                        maxlen=15,
-                                        batch_size=32,
-                                        valid_batch_size=32,
-					datasets=['%s/europarl-v7.fr-en.fr.tok'%basedir,
-					'%s/europarl-v7.fr-en.en.tok'%basedir],
-					valid_datasets=['%s/newstest2011.fr.tok'%basedir,
-					'%s/newstest2011.en.tok'%basedir],
-					dictionaries=['%s/europarl-v7.fr-en.fr.tok.pkl'%basedir,
-					'%s/europarl-v7.fr-en.en.tok.pkl'%basedir],
-                                        validFreq=500000,
-                                        dispFreq=1,
-                                        saveFreq=100,
-                                        sampleFreq=50,
-                                        use_dropout=params['use-dropout'][0])
+def main(do_test=0, use_dropout=0):
+    saveto = 'model_session3_use_dropout=%s.npz'%str(use_dropout)
+    print saveto
+    if do_test:
+        from nmt import test
+        f = test
+    else:
+        from nmt import train
+        f = train
+    validerr = f(saveto=saveto,
+                 reload_=True, # Remove saveto to disable reloading
+                 use_dropout=use_dropout,
+                 dim_word=150,
+                 dim=124,
+                 n_words=28, n_words_src=28,
+                 decay_c=0.,
+                 clip_c=1.,
+                 lrate=1e-4,
+                 optimizer='adadelta',
+                 maxlen=25,
+                 batch_size=32,
+                 valid_batch_size=32,
+                 datasets=DATASETS,
+                 valid_datasets=VALID_DATASETS,
+                 dictionaries=DICTIONARIES,
+                 validFreq=500,
+                 dispFreq=50,
+                 saveFreq=500,
+                 sampleFreq=500,
+                 )
     return validerr
 
 if __name__ == '__main__':
-    basedir = '/data/lisatmp3/firatorh/nmt/europarlv7'
-    main(0, {
-        'model': ['%s/models/model_session3.npz'%basedir],
-        'dim_word': [150],
-        'dim': [124],
-        'n-words': [3000],
-        'optimizer': ['adadelta'],
-        'decay-c': [0.],
-        'clip-c': [1.],
-        'use-dropout': [False],
-        'learning-rate': [0.0001],
-        'reload': [False]})
-
-
+    import argparse
+    arg_parser = argparse.ArgumentParser(description='')
+    arg_parser.add_argument('--test', default=0, type=int, help='Default={0}')
+    arg_parser.add_argument('--dropout', default=0, type=int, help='Default={0}')
+    args=arg_parser.parse_args()
+    main(args.test, args.dropout)
+    if not args.test:
+        main(True, args.dropout)
